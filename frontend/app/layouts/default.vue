1e8228bf2a015c291197db1b838730025b8fe51a
Jérôme Castagné <jerome.raez@gmail.com>
2025-10-16 15:25:13 +0200

    add campaign logo

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -9,6 +9,9 @@ const route = useRoute();
     ]"
   >
     <main class="page max-w-5xl my-[10dvh]">
+      <div class="fixed top-6 left-6">
+        <img src="~/assets/img/logo.svg" alt="ICF Next" />
+      </div>
       <div v-if="route.meta.hideAbout !== true" class="fixed top-6 right-6">
         <UButton color="neutral" variant="ghost" to="/about">About</UButton>
       </div>
